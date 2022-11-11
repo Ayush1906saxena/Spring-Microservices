7c1d09a491ff4a9e67a02f884a74cf747a1eca1c
Ayush Saxena <ayushsaxenarockz1906@gmail.com>
2022-11-11 09:52:00 +0530

    Added a very simple rest-controller.

---
+++ src/main/java/com/ayush/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.ayush.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+    String message;
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
