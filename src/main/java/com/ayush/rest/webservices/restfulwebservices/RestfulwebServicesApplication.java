d29b5ff57e54cb97a534025d8a97094619d71eec
Ayush Saxena <ayushsaxenarockz1906@gmail.com>
2022-11-11 09:27:22 +0530

    first commit

---
+++ src/main/java/com/ayush/rest/webservices/restfulwebservices/RestfulwebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ayush.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulwebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulwebServicesApplication.class, args);
+	}
+
+}
