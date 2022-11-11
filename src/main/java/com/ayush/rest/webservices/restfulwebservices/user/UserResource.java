44156c081a5cc55577053926c64220a8dd29d981
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2022-11-11 21:08:12 +0530

    Added hateoas library and implemented it.

--- src/main/java/com/ayush/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/ayush/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,6 +1,8 @@
 package com.ayush.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -8,6 +10,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -20,11 +25,17 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public Resource<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
         if (user == null)
             throw new UserNotFoundException("id-"+id);
-        return user;
+
+        // HATEOAS : adding a link to all users
+        Resource<User> resource = new Resource<User>(user);
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
     }
 
     @PostMapping("/users")
